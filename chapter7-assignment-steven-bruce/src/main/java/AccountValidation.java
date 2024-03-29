7e11ebb261ac4f69f3319637b73f5c633d215098
Steven Bruce <bruces92@gmail.com>
2024-03-28 20:21:29 -0400

    Completed second task in assignment 7

--- chapter7-assignment-steven-bruce/src/main/java/AccountValidation.java
+++ chapter7-assignment-steven-bruce/src/main/java/AccountValidation.java
@@ -1,2 +1,45 @@
 public class AccountValidation {
+    // Field
+    private final int[][] accountNumbers = new int[3][6];
+
+    /**
+     * Constructor with no params, fills out the whole array with static values.
+     */
+    public AccountValidation() {
+        accountNumbers[0][0] = 5658845;
+        accountNumbers[0][1] = 4520125;
+        accountNumbers[0][2] = 7895122;
+        accountNumbers[0][3] = 8777541;
+        accountNumbers[0][4] = 8451277;
+        accountNumbers[0][5] = 1302850;
+        accountNumbers[1][0] = 8080152;
+        accountNumbers[1][1] = 4562555;
+        accountNumbers[1][2] = 5552012;
+        accountNumbers[1][3] = 5050552;
+        accountNumbers[1][4] = 7825877;
+        accountNumbers[1][5] = 1250255;
+        accountNumbers[2][0] = 1005231;
+        accountNumbers[2][1] = 6545231;
+        accountNumbers[2][2] = 3852085;
+        accountNumbers[2][3] = 7576651;
+        accountNumbers[2][4] = 7881200;
+        accountNumbers[2][5] = 4581002;
+    }
+
+    /**
+     *  Loops through the 2D array row by row, then nested loop to go through each element in the row,
+     *   and repeats until a match is found or every element in the array has been checked.
+     * @param testNumber User entered number to compare against the accountNumbers.
+     * @return a boolean, true if there is a match, false if not.
+     */
+    public boolean validate(int testNumber) {
+        for (int[] accountNumbersRow : accountNumbers) {
+            for (int accountNumber : accountNumbersRow) {
+                if (testNumber == accountNumber) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
 }
--- chapter7-assignment-steven-bruce/src/main/java/AccountValidation_Test.java
+++ chapter7-assignment-steven-bruce/src/main/java/AccountValidation_Test.java
@@ -1,2 +1,19 @@
+import java.util.Scanner;
+
 public class AccountValidation_Test {
+    public static void main(String[] args) {
+        // Creates an AccountValidation object, so we can call its method validate later.
+        AccountValidation acctsDatabase = new AccountValidation();
+        // Takes user input of a number to test against the AccountValidation array.
+        Scanner numpad = new Scanner(System.in);
+        System.out.println("Enter your 7 digit account number for validation");
+        int acctNum = numpad.nextInt();
+
+        // Calls validate method, and print if the number is valid or not based off the result true or false.
+        if (acctsDatabase.validate(acctNum)) {
+            System.out.println("Your account number " + acctNum + " is valid!");
+        } else {
+            System.out.println("Your account number was not able to be validated.");
+        }
+    }
 }
